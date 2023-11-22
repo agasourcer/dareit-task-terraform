54f1baaa6ddb5b966ce9be3c27772da6a0c61bde
agasourcer <a.rachwalak@gmail.com>
2023-11-22 21:06:57 +0100

    feat: adding terraform files
    Adding CI/CD pipelines with terraform

---
+++ backend.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_version = ">= 1.0.11"
+  backend "gcs" {
+    bucket = "aga-terraform-state-file"
+    prefix = "dev"
+  }
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.41.0"
+    }
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = "4.41.0"
+    }
+  }
---
+++ provider.tf
@@ -0,0 +1,5 @@
+provider "google" {
+  project = "kinetic-anvil-400904"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
